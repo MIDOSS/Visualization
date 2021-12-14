6bb9c42baaf2d2e54b89144f48ca5eaf5bf8b84d
Susan Allen <sallen@eoas.ubc.ca>
2021-12-13 20:41:51 -0500

    After 1600 runs

--- notebooks/Aggregate.sh
+++ notebooks/Aggregate.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=eighth_aggregate
+#SBATCH --job-name=tenth_aggregate
 #SBATCH --mem=40G
 #SBATCH --time=8:00:00
 #SBATCH --mail-user=sallen@eoas.ubc.ca
 #SBATCH --mail-type=ALL
 #SBATCH --account=rrg-allen
 # stdout and stderr file paths/names
-#SBATCH --output=/scratch/allen/MIDOSS/aggregate/eighth_stdout
-#SBATCH --error=/scratch/allen/MIDOSS/aggregate/eighth_stderr
+#SBATCH --output=/scratch/allen/MIDOSS/aggregate/tenth_stdout
+#SBATCH --error=/scratch/allen/MIDOSS/aggregate/tenth_stderr
 
 
 module load python/3.8.2
@@ -16,4 +16,4 @@ source ~/venvs/jupyter/bin/activate
 
 cd /scratch/allen/MIDOSS/aggregate
 
-python3 -m Incremental_Sums seventh_200 eighth_200 /scratch/dlatorne/MIDOSS/runs/monte-carlo/8-200_near-BP_spill-hr_2021-12-12T131941 False
+python3 -m Incremental_Sums ninth_200 tenth_200 /scratch/dlatorne/MIDOSS/runs/monte-carlo/10-200_near-BP_spill-hr_2021-12-12T132003 False
