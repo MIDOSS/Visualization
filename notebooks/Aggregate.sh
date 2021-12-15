9bd2fb36ccc5bbe44ae68d556d78e209d61342bd
Susan Allen <sallen@eoas.ubc.ca>
2021-12-15 18:39:26 -0500

    Look at deep distribution of one file

--- notebooks/Aggregate.sh
+++ notebooks/Aggregate.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=tenth_aggregate
+#SBATCH --job-name=eleventh_aggregate
 #SBATCH --mem=40G
 #SBATCH --time=8:00:00
 #SBATCH --mail-user=sallen@eoas.ubc.ca
 #SBATCH --mail-type=ALL
 #SBATCH --account=rrg-allen
 # stdout and stderr file paths/names
-#SBATCH --output=/scratch/allen/MIDOSS/aggregate/tenth_stdout
-#SBATCH --error=/scratch/allen/MIDOSS/aggregate/tenth_stderr
+#SBATCH --output=/scratch/allen/MIDOSS/aggregate/eleventh_stdout
+#SBATCH --error=/scratch/allen/MIDOSS/aggregate/eleventh_stderr
 
 
 module load python/3.8.2
@@ -16,4 +16,4 @@ source ~/venvs/jupyter/bin/activate
 
 cd /scratch/allen/MIDOSS/aggregate
 
-python3 -m Incremental_Sums ninth_200 tenth_200 /scratch/dlatorne/MIDOSS/runs/monte-carlo/10-200_near-BP_spill-hr_2021-12-12T132003 False
+python3 -m Incremental_Sums tenth_200 eleventh_200 /scratch/dlatorne/MIDOSS/runs/monte-carlo/11-200_near-BP_spill-hr_2021-12-14T172256 False
