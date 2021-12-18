00a65b2cee3d8dd694aac5415ea5c96b66c849eb
Susan Allen <sallen@eoas.ubc.ca>
2021-12-17 20:44:44 -0500

    with 2600 spills

--- notebooks/Aggregate.sh
+++ notebooks/Aggregate.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=twentieth_aggregate
+#SBATCH --job-name=twentyfirst_aggregate
 #SBATCH --mem=40G
 #SBATCH --time=8:00:00
 #SBATCH --mail-user=sallen@eoas.ubc.ca
 #SBATCH --mail-type=ALL
 #SBATCH --account=rrg-allen
 # stdout and stderr file paths/names
-#SBATCH --output=/scratch/allen/MIDOSS/aggregate/twentieth_stdout
-#SBATCH --error=/scratch/allen/MIDOSS/aggregate/twentieth_stderr
+#SBATCH --output=/scratch/allen/MIDOSS/aggregate/twentyfirst_stdout
+#SBATCH --error=/scratch/allen/MIDOSS/aggregate/twentyfirst_stderr
 
 
 module load python/3.8.2
@@ -16,4 +16,4 @@ source ~/venvs/jupyter/bin/activate
 
 cd /scratch/allen/MIDOSS/aggregate
 
-python3 -m Incremental_Sums nineteenth_200 twentieth_200 /scratch/dlatorne/MIDOSS/runs/monte-carlo/20-200_near-BP_spill-hr_2021-12-13T170716 False
+python3 -m Incremental_Sums twentieth_200 twentyfirst_200 /scratch/dlatorne/MIDOSS/runs/monte-carlo/21-200_near-BP_spill-hr_2021-12-16T222516 False
