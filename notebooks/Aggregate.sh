191b43cc40a9a07e09ae28601b15a3c8bfd52f40
Susan Allen <sallen@eoas.ubc.ca>
2021-12-22 12:54:37 -0500

    To 32nd 200

--- notebooks/Aggregate.sh
+++ notebooks/Aggregate.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=twentysecond_aggregate
+#SBATCH --job-name=thirtysecond_aggregate
 #SBATCH --mem=40G
 #SBATCH --time=8:00:00
 #SBATCH --mail-user=sallen@eoas.ubc.ca
 #SBATCH --mail-type=ALL
 #SBATCH --account=rrg-allen
 # stdout and stderr file paths/names
-#SBATCH --output=/scratch/allen/MIDOSS/aggregate/twentysecond_stdout
-#SBATCH --error=/scratch/allen/MIDOSS/aggregate/twentysecond_stderr
+#SBATCH --output=/scratch/allen/MIDOSS/aggregate/thirtysecond_stdout
+#SBATCH --error=/scratch/allen/MIDOSS/aggregate/thirtysecond_stderr
 
 
 module load python/3.8.2
@@ -16,4 +16,4 @@ source ~/venvs/jupyter/bin/activate
 
 cd /scratch/allen/MIDOSS/aggregate
 
-python3 -m Incremental_Sums twentyfirst_200 twentysecond_200 /scratch/dlatorne/MIDOSS/runs/monte-carlo/22-200_near-BP_spill-hr_2021-12-17T135814 False
+python3 -m Incremental_Sums thirtyfirst_200 thirtysecond_200 /scratch/dlatorne/MIDOSS/runs/monte-carlo/32-200_near-BP_spill-hr_2021-12-20T163341 False
