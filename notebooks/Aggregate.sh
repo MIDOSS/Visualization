a8d99cdcd1365e543eccd2604303a364ce17a6dd
Susan Allen <sallen@eoas.ubc.ca>
2022-06-02 15:12:22 -0400

    Add beaching files to the aggregation process

--- notebooks/Aggregate.sh
+++ notebooks/Aggregate.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-#SBATCH --job-name=fortyeighth_aggregate
+#SBATCH --job-name=fiftieth_aggregate
 #SBATCH --mem=40G
 #SBATCH --time=8:00:00
 #SBATCH --mail-user=sallen@eoas.ubc.ca
 #SBATCH --mail-type=ALL
 #SBATCH --account=rrg-allen
 # stdout and stderr file paths/names
-#SBATCH --output=/scratch/allen/MIDOSS/aggregate/fortyeighth_stdout
-#SBATCH --error=/scratch/allen/MIDOSS/aggregate/fortyeighth_stderr
+#SBATCH --output=/scratch/allen/MIDOSS/aggregate/fiftieth_stdout
+#SBATCH --error=/scratch/allen/MIDOSS/aggregate/fiftieth_stderr
 
+RESULTS_DIR="/scratch/dlatorne/MIDOSS/runs/monte-carlo/50-200_near-BP_try3_2022-05-23T130812"
 
 module load python/3.8.2
 source ~/venvs/jupyter/bin/activate
 
 cd /scratch/allen/MIDOSS/aggregate
 
-python3 -m Incremental_Sums fortyseventh_200 fortyeighth_200 /scratch/dlatorne/MIDOSS/runs/monte-carlo/48-200_near-BP_spill-hr_2021-12-25T135612 False
+python3 -m SaveBeaching ${RESULTS_DIR}/results/
+python3 -m Incremental_Sums fortyninth_200 fiftieth_200 ${RESULTS_DIR} False
+
